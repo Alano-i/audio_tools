953e318981ee56291cc1765d76dd88dbab2d14d4
Alano-i <68833595+Alano-i@users.noreply.github.com>
2024-02-20 02:39:54 +0800

    Update index.jsx

--- frontend/src/pages/settings/index.jsx
+++ frontend/src/pages/settings/index.jsx
@@ -1,46 +1,50 @@
+import React, { useState } from "react";
 const Index = () => {
+  const [audio_paths, set_audio_paths] = useState("https://qyapi.weixin.qq.com");
+
+
   return (
     <div className="min-w-[800px] flex flex-col gap-4">
       <label className="form-control w-full">
         <div className="label">
-          <span className="w-fulllabel-text">通知推送渠道</span>
+          <span className="label-text text-opacity-50 text-opacity-50">通知推送渠道</span>
         </div>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+        <input type="text" placeholder="" className="input input-bordered w-full " />
       </label>
 
       <label className="form-control w-full">
         <div className="label">
-          <span className="w-fulllabel-text">企业微信 API 代理</span>
+          <span className="label-text text-opacity-50 text-opacity-50">企业微信 API 代理</span>
         </div>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+        <input type="text" placeholder="https://qyapi.weixin.qq.com" className="input input-bordered w-full " />
       </label>
 
       <label className="form-control w-full">
         <div className="label">
-          <span className="w-fulllabel-text">通知消息默认封面</span>
+          <span className="label-text text-opacity-50 text-opacity-50">通知消息默认封面</span>
         </div>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+        <input type="text" placeholder="" className="input input-bordered w-full " />
       </label>
 
       <label className="form-control w-full">
         <div className="label">
-          <span className="w-fulllabel-text">有声书父文件夹</span>
+          <span className="label-text text-opacity-50 text-opacity-50">有声书父文件夹</span>
         </div>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+        <input type="text" placeholder="" className="input input-bordered w-full " />
       </label>
 
       <label className="form-control w-full">
         <div className="label">
-          <span className="w-fulllabel-text">音乐父文件夹</span>
+          <span className="label-text text-opacity-50 text-opacity-50">音乐父文件夹</span>
         </div>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+        <input type="text" placeholder="" className="input input-bordered w-full " />
       </label>
 
       <label className="form-control w-full">
         <div className="label">
-          <span className="w-fulllabel-text">有声书下载完成接口</span>
+          <span className="label-text text-opacity-50 text-opacity-50">有声书下载完成接口</span>
         </div>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+        <input type="text" placeholder="" className="input input-bordered w-full " />
       </label>
 
       <div className="mt-8 btn btn-primary">测试设置</div>
