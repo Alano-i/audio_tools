8b449b8800d08c393b17ce1bef23788ab05110e0
Alano-i <68833595+Alano-i@users.noreply.github.com>
2024-02-20 13:44:28 +0800

    画输入框

--- frontend/src/pages/organize/index.jsx
+++ frontend/src/pages/organize/index.jsx
@@ -12,9 +12,9 @@ const Index = () => {
   const [podcast_author, set_podcast_author] = useState("");
   const [reader, set_reader] = useState("");
   const [pub_year, set_pub_year] = useState("");
-  const [is_group, set_is_group] = useState("True");
-  const [short_filename_config, set_short_filename_config] = useState("True");
-  const [deep_config, set_deep_config] = useState("True");
+  const [is_group, set_is_group] = useState(true);
+  const [short_filename_config, set_short_filename_config] = useState(true);
+  const [deep_config, set_deep_config] = useState(false);
 
   const handleMainOptionChange = (option) => {
     setSelectedMainOption(option);
@@ -33,6 +33,9 @@ const Index = () => {
     console.log("作者:", podcast_author);
     console.log("演播者:", reader);
     console.log("发布年份:", pub_year);
+    console.log("强制200集:", is_group);
+    console.log("根据标题优化:", short_filename_config);
+    console.log("深路径:", deep_config);
   };
 
   const mainOptions = [
@@ -61,6 +64,9 @@ const Index = () => {
           value={selectedMainOption}
           onChange={(e) => handleMainOptionChange(e.target.value)}
         >
+          <option value="" disabled>
+            请选择要执行的操作
+          </option>
           {mainOptions.map((option) => (
             <option key={option.value} value={option.value}>
               {option.label}
@@ -101,8 +107,29 @@ const Index = () => {
 
       {selectedMainOption === "generate_podcast" &&
         selectedRadioOption === "audio_book" && (
-          <div className="min-w-[800px] flex flex-col gap-4">
+          <div className="w-full flex flex-col gap-4">
+            {/*  带说明提示是输入框 */}
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text text-opacity-50">
+                  输入文件夹名称或完整路径
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder=""
+                className="input input-bordered w-full"
+                value={audio_paths}
+                onChange={(e) => set_audio_paths(e.target.value)}
+              />
+              <div className="label">
+                <span className="label-text-alt text-opacity-50">
+                  支持多条，一行一条 /Media/有声书/
+                </span>
+              </div>
+            </label>
 
+            <div className="flex gap-4">
             <label className="form-control w-full">
               <div className="label">
                 <span className="w-full label-text text-opacity-50">书名</span>
@@ -115,93 +142,111 @@ const Index = () => {
                 onChange={(e) => set_book_title(e.target.value)}
               />
             </label>
+            <label className="form-control w-full">
+                <div className="label">
+                  <span className="w-full label-text text-opacity-50">
+                    作者
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  placeholder=""
+                  className="input input-bordered w-full"
+                  value={podcast_author}
+                  onChange={(e) => set_podcast_author(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="flex gap-4">
 
-            {/*  带说明提示是输入框 */}
+
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="w-full label-text text-opacity-50">
+                    演播者
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  placeholder=""
+                  className="input input-bordered w-full"
+                  value={reader}
+                  onChange={(e) => set_reader(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="w-full label-text text-opacity-50">
+                    发布年份
+                  </span>
+                </div>
+                <input
+                  type="number"
+                  placeholder=""
+                  className="input input-bordered w-full"
+                  value={pub_year}
+                  onChange={(e) => set_pub_year(e.target.value)}
+                />
+              </label>
+
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="w-full label-text text-opacity-50">
+                    分类
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  placeholder=""
+                  className="input input-bordered w-full"
+                  value={podcast_category}
+                  onChange={(e) => set_podcast_category(e.target.value)}
+                />
+              </label>
+            </div>
             <label className="form-control w-full">
               <div className="label">
-                <span className="label-text text-opacity-50">输入文件夹名称或完整路径</span>
+                <span className="label-text text-opacity-50">简介</span>
+                {/* <span className="label-text-alt">Alt label</span> */}
               </div>
-              <input
-                type="text"
+              <textarea
+                className="textarea textarea-bordered h-16 w-full"
                 placeholder=""
-                className="input input-bordered w-full"
-                value={audio_paths}
-                onChange={(e) => set_audio_paths(e.target.value)}
-              />
-              <div className="label">
-                <span className="label-text-alt text-opacity-50">
-                支持多条，一行一条 /Media/有声书/
-                </span>
-              </div>
-            </label>
-
-            <label className="form-control w-full">
-              <div className="label">
-                <span className="w-full label-text text-opacity-50">简介</span>
-              </div>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered w-full"
                 value={podcast_summary}
                 onChange={(e) => set_podcast_summary(e.target.value)}
-              />
+              ></textarea>
             </label>
+            <div className="flex gap-4 mb-4 mt-4">
+              <label className="cursor-pointer p-0 m-0 label">
+                <input
+                  type="checkbox"
+                  className="checkbox checkbox-primary"
+                  checked={is_group}
+                  onChange={(e) => set_is_group(e.target.checked)}
+                />
+                <span className="label-text ml-2">第一季强制200集</span>
+              </label>
 
-            <label className="form-control w-full">
-              <div className="label">
-                <span className="w-full label-text text-opacity-50">分类</span>
-              </div>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered w-full"
-                value={podcast_category}
-                onChange={(e) => set_podcast_category(e.target.value)}
-              />
-            </label>
+              <label className="cursor-pointer p-0 m-0 label">
+                <input
+                  type="checkbox"
+                  className="checkbox checkbox-primary"
+                  checked={short_filename_config}
+                  onChange={(e) => set_short_filename_config(e.target.checked)}
+                />
+                <span className="label-text ml-2">根据文件名优化每集标题</span>
+              </label>
 
-            <label className="form-control w-full">
-              <div className="label">
-                <span className="w-full label-text text-opacity-50">作者</span>
-              </div>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered w-full"
-                value={podcast_author}
-                onChange={(e) => set_podcast_author(e.target.value)}
-              />
-            </label>
-
-            <label className="form-control w-full">
-              <div className="label">
-                <span className="w-full label-text text-opacity-50">演播者</span>
-              </div>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered w-full"
-                value={reader}
-                onChange={(e) => set_reader(e.target.value)}
-              />
-            </label>
-
-            <label className="form-control w-full">
-              <div className="label">
-                <span className="w-full label-text text-opacity-50">发布年份</span>
-              </div>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered w-full"
-                value={pub_year}
-                onChange={(e) => set_pub_year(e.target.value)}
-              />
-            </label>
-
-
-
+              <label className="cursor-pointer p-0 m-0 label">
+                <input
+                  type="checkbox"
+                  className="checkbox checkbox-primary"
+                  checked={deep_config}
+                  onChange={(e) => set_deep_config(e.target.checked)}
+                />
+                <span className="label-text ml-2">深路径</span>
+              </label>
+            </div>
           </div>
         )}
       {selectedMainOption === "generate_podcast" &&
@@ -240,8 +285,8 @@ const Index = () => {
             </div>
           </label>
         )}
-      <div className="mt-4 btn btn-primary" onClick={handleTestSetting}>
-        测试设置
+      <div className="mt-2 mb-10 btn btn-primary" onClick={handleTestSetting}>
+        运行
       </div>
     </div>
   );
